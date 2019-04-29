ab7ad6efdda0f7e9353d75fc07bead02aa227e9e
cahaug <caleahaug@gmail.com>
2019-04-28 17:48:04 -0700

    mvp complete on prototypes.js, started on classes.js

--- challenges/classes.js
+++ challenges/classes.js
@@ -1,5 +1,15 @@
 // 1. Copy and paste your prototype in here and refactor into class syntax.
 
+const CuboidMaker = function (props){
+    this.length=props.length;
+    this.width=props.width;
+    this.height=props.height;
+  }
+
+  CuboidMaker.prototype.volume = function(cuboid){
+    return this.length*this.width*this.height;
+  }
+
 // Test your volume and surfaceArea methods by uncommenting the logs below:
 // console.log(cuboid.volume()); // 100
 // console.log(cuboid.surfaceArea()); // 130
--- challenges/functions.js
+++ challenges/functions.js
@@ -7,6 +7,10 @@
   * In the body of the function return the callback with the two parameters that you created
 */
 
+  function consume (a, b, cb){
+    return cb(a,b)
+  }
+
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
@@ -14,18 +18,32 @@
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
 
+function add (a,b){
+  return a+b;
+}
+function multiply (a,b){
+  return a*b;
+}
+function greeting (firstName, lastName){
+  return `Hello ${firstName} ${lastName}, nice to meet you!`;
+}
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2,2,add)); // 4
+console.log(consume(10,16,multiply)); // 160
+console.log(consume("Mary","Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
 
-// Explain in your own words why `nestedfunction()` can access the variable `internal`.
+// Explain in your own words why `nestedFunction()` can access the variable `internal`.
 
 // Explanation: 
+
+  /**
+  nestedFunction() can access `internal` because of Javascript closures.  Nested functions can always pull variables from any function they are children of and any other global variables, but the opposite is not true.  Any other functions not nested in myFunction or nestedFunction cannot access internal, it is private because of the closures.
+
+   */
 
 
 const external = "I'm outside the function";
